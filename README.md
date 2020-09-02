76b4ceb8ad93ad7b996845a4e5d367da643f81a5
李秋奇 <mail@qiuqi.li>
2020-09-02 10:39:37 +0800

    Project Init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Horizon-Cli
+# Horizon-UI-cli
 用于在项目中添加Horizon UI组件库的脚手架
---
+++ exec.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+const chalk = require('chalk')
+const inquirer = require('inquirer');
+const cmdify = require('cmdify');
+const {spawn, exec} = require('child_process');
+const WELCOME = chalk.blue(
+  '┌─────────────────────────────────────────────┐\n' +
+  '│                                             │\n' +
+  '│          Welcome to Horizon UI CLI          │\n' +
+  '│                                             │\n' +
+  '└─────────────────────────────────────────────┘\n'
+)
+const SUCCESS = chalk.green(
+  '┌─────────────────────────────────────────────┐\n' +
+  '│                                             │\n' +
+  '│     Horizon UI Installed Successfully!      │\n' +
+  '│                                             │\n' +
+  '└─────────────────────────────────────────────┘\n'
+)
+const COMING_SOON = chalk.yellow(
+  '┌─────────────────────────────────────────────┐\n' +
+  '│                                             │\n' +
+  '│               Coming Soon...                │\n' +
+  '│                                             │\n' +
+  '└─────────────────────────────────────────────┘\n'
+)
+
+var questions = [
+  {
+    type: 'list',
+    name: 'target',
+    message: '请选择要将Horizon UI用于哪种项目',
+    choices: ['H5', 'uni-app'],
+  },
+  {
+    type: 'list',
+    name: 'css_unit',
+    message: '请选择想要使用的css单位',
+    choices: ['rem', 'px'],
+    when: answer => {
+      return answer.target === 'H5'
+    }
+  },
+];
+
+function run(argv) {
+  switch (argv[0]) {
+    case 'init':
+      console.log(WELCOME)
+      inquirer.prompt(questions).then((answers) => {
+        switch (answers.target) {
+          case 'H5':
+            switch (answers.css_unit) {
+              case 'rem':
+                // const cmd = 'yarn add horizon-ui'
+                // const ls = exec(cmd, function(error, stdout, stderr) {
+                //   console.log(error, stdout, stderr)
+                // })
+                // ls.stdout.on('data', data => {
+                //   console.log('stdout')
+                //   console.log(data)
+                // })
+                // ls.stderr.on('data', data => {
+                //   console.log('stderr')
+                //   console.log(data)
+                // })
+                // ls.on('close', code => {
+                //   console.log('close')
+                //   console.log(`child process exited with code ${code}`)
+                // })
+                spawn(cmdify('yarn'), ['add', 'horizon-ui'], {
+                  stdio: 'inherit'
+                }).on('close', code => {
+                  code === 0 && console.log(SUCCESS)
+                })
+                break
+              case 'px':
+                console.log(COMING_SOON)
+                break
+            }
+            break
+          case 'uni-app':
+            console.log(COMING_SOON)
+            break
+        }
+      });
+      break
+    default:
+      console.log(chalk.red('error'), chalk.blue('缺少参数'), chalk.yellow('[init]'), chalk.blue('或参数有误，请重试'))
+  }
+}
+
+run(process.argv.slice(2));
